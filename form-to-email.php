ed268eeb2c439074bc395504670ce5bf95b2bf17
Pakkasmaria <mayelia.pakkanen@gmail.com>
2021-04-22 17:31:32 +0300

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -1,8 +1,3 @@
 <?php
-	$email_from = 'yourname@yourwebsite.com';
-
-	$email_subject = "New Form submission";
-
-	$email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
+	echo "TEKSTIÄÄ";
 ?>
